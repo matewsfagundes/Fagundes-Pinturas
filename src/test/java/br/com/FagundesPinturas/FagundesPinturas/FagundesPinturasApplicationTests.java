30e5a7d4ffbf62429ba0f0fea230ad0b30640397
Matheus Fagundes <matewsfagundes@gmail.com>
2022-12-19 21:28:16 -0300

    inicia projeto

---
+++ src/test/java/br/com/FagundesPinturas/FagundesPinturas/FagundesPinturasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.FagundesPinturas.FagundesPinturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FagundesPinturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

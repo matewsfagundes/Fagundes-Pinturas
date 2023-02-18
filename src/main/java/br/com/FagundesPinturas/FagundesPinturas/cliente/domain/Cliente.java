71fe0c1f08173cc6f78cd5fef3908fb0ec8d7dfd
Matheus Fagundes <matewsfagundes@gmail.com>
2023-02-18 18:24:57 -0300

    Altera imports cliente domain

--- src/main/java/br/com/FagundesPinturas/FagundesPinturas/cliente/domain/Cliente.java
+++ src/main/java/br/com/FagundesPinturas/FagundesPinturas/cliente/domain/Cliente.java
@@ -3,14 +3,14 @@ package br.com.FagundesPinturas.FagundesPinturas.cliente.domain;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,7 +26,6 @@ public class Cliente {
 	@NotBlank
 	private String nomeCompleto;
 	@NotBlank
-	@Email
 	private String email;
 	@NotBlank
 	private String whatsApp;

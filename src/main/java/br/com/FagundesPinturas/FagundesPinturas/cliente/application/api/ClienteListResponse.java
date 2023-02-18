c16a9df2d8a26b2bd647cb0f2bbb8195e3728d9d
Matheus Fagundes <matewsfagundes@gmail.com>
2023-02-18 19:59:38 -0300

    finaliza metodo busca todos os clientes implementando infra e criando stream

--- src/main/java/br/com/FagundesPinturas/FagundesPinturas/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/FagundesPinturas/FagundesPinturas/cliente/application/api/ClienteListResponse.java
@@ -2,6 +2,7 @@ package br.com.FagundesPinturas.FagundesPinturas.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.FagundesPinturas.FagundesPinturas.cliente.domain.Cliente;
 import lombok.Value;
@@ -12,7 +13,18 @@ public class ClienteListResponse {
 	private String email;
 	private String whatsApp;
 	private String cpfCnpj;
+	
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+		return clientes.stream()
+				.map(ClienteListResponse::new)
+				.collect(Collectors.toList());
+	}
+
+	private ClienteListResponse(Cliente cliente) {
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.email = cliente.getEmail();
+		this.whatsApp = cliente.getWhatsApp();
+		this.cpfCnpj = cliente.getCpfCnpj();
 	}
 }

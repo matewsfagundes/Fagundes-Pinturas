1115907899b7aa7569e0ed3541d36bc698e3d4a1
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-17 18:55:40 -0300

    inicia projeto

---
+++ src/main/java/br/com/fagundespinturas/fagundespinturas/FagundesPinturasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fagundespinturas.fagundespinturas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FagundesPinturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FagundesPinturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fagundespinturas/fagundespinturas/FagundesPinturasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fagundespinturas.fagundespinturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FagundesPinturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

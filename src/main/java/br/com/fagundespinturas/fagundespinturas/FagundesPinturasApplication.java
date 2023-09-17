acda1b2a71dbf5e6394e62e29cd5598328671831
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-17 19:09:03 -0300

    teste web ok

--- src/main/java/br/com/fagundespinturas/fagundespinturas/FagundesPinturasApplication.java
+++ src/main/java/br/com/fagundespinturas/fagundespinturas/FagundesPinturasApplication.java
@@ -2,12 +2,20 @@ package br.com.fagundespinturas.fagundespinturas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class FagundesPinturasApplication {
-
+	@GetMapping
+	public String getHomeTest() {
+		return "Fagundes pinturas - API Home";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FagundesPinturasApplication.class, args);
 	}
-
 }

31da02a40dc0c793563185bc49d943d7cdd53632
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-17 22:08:05 -0300

    ajusta mapeamneto UUID

--- src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/domain/Cliente.java
+++ src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/domain/Cliente.java
@@ -5,7 +5,10 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.br.CPF;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -13,11 +16,14 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
 @Entity
 public class Cliente {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
 	private UUID idCliente;
 	@NotBlank
 	private String nomeCompleto;
@@ -31,9 +37,9 @@ public class Cliente {
 	private LocalDate dataNascimento;
 	@CPF
 	private String cpf;
-	private Cliente(@NotBlank String nomeCompleto, @NotBlank @Email String email,
-			@NotBlank String celular, Sexo sexo, @NotNull LocalDate dataNascimento, @CPF String cpf) {
-		this.idCliente = UUID.randomUUID();
+
+	private Cliente(@NotBlank String nomeCompleto, @NotBlank @Email String email, @NotBlank String celular, Sexo sexo,
+			@NotNull LocalDate dataNascimento, @CPF String cpf) {
 		this.nomeCompleto = nomeCompleto;
 		this.email = email;
 		this.celular = celular;

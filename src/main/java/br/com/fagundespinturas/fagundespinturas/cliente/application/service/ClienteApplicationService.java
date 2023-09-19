f6ef5d1ab5bafc8d5e0839c67dd1dbedda9c89a2
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-18 22:14:07 -0300

    implementa ClienteApplicationService e corrige ClienteApi

---
+++ src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/application/service/ClienteApplicationService.java
@@ -0,0 +1,18 @@
+package br.com.fagundespinturas.fagundespinturas.cliente.application.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.fagundespinturas.fagundespinturas.cliente.application.api.ClienteRequest;
+import br.com.fagundespinturas.fagundespinturas.cliente.application.api.ClienteResponse;
+import lombok.extern.log4j.Log4j2;
+@Service
+@Log4j2
+public class ClienteApplicationService implements ClienteService {
+
+	@Override
+	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
+		log.info("[inicia] ClienteApplicationService - criaCliente");
+		log.info("[finaliza] ClienteApplicationService - criaCliente");
+		return null;
+	}
+}

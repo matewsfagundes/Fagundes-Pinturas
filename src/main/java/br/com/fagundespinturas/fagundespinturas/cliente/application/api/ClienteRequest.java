f967e5fe3bdea3e818dea30bfba4554821eac861
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-18 22:01:43 -0300

    implementa request e response

--- src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,16 @@
 package br.com.fagundespinturas.fagundespinturas.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
 
+import br.com.fagundespinturas.fagundespinturas.cliente.domain.Sexo;
+import lombok.Value;
+
+@Value
+public class ClienteRequest {
+	private String nomeCompleto;
+	private String email;
+	private String celular;
+	private Sexo sexo;
+	private LocalDate dataNascimento;
+	private String cpf;
 }

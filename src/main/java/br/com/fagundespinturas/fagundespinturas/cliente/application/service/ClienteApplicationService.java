8ba5415d367ead826ceba3156c1ed9aa5954139c
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-20 20:42:33 -0300

    implementa fluxo service, teste ok

--- src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/application/service/ClienteApplicationService.java
@@ -1,7 +1,10 @@
 package br.com.fagundespinturas.fagundespinturas.cliente.application.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import br.com.fagundespinturas.fagundespinturas.cliente.application.api.ClienteListResponse;
 import br.com.fagundespinturas.fagundespinturas.cliente.application.api.ClienteRequest;
 import br.com.fagundespinturas.fagundespinturas.cliente.application.api.ClienteResponse;
 import br.com.fagundespinturas.fagundespinturas.cliente.application.repository.ClienteRepository;
@@ -23,5 +26,12 @@ public class ClienteApplicationService implements ClienteService {
 		return ClienteResponse.builder()
 				.idCliente(cliente.getIdCliente())
 				.build();
+	}
+
+	@Override
+	public List<ClienteListResponse> buscaTodosClientes() {
+		log.info("[inicia] ClienteApplicationService - buscaTodosClientes");
+		log.info("[finaliza] ClienteApplicationService - buscaTodosClientes");
+		return null;
 	}
 }

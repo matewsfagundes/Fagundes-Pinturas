e18df5b1c4117e19af85d18dcbe33e3b4e7d1f70
Matheus Fagundes <matewsfagundes@gmail.com>
2023-09-20 21:05:46 -0300

    implementa getTodos

--- src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/application/api/ClienteListResponse.java
+++ src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/application/api/ClienteListResponse.java
@@ -2,9 +2,12 @@ package br.com.fagundespinturas.fagundespinturas.cliente.application.api;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import br.com.fagundespinturas.fagundespinturas.cliente.domain.Cliente;
+import lombok.Value;
 
+@Value
 public class ClienteListResponse {
 	private UUID idCliente;
 	private String nomeCompleto;
@@ -13,6 +16,15 @@ public class ClienteListResponse {
 	private String cpf;
 
 	public static List<ClienteListResponse> converte(List<Cliente> clientes) {
-		return null;
+		return clientes.stream().map(ClienteListResponse::new).collect(Collectors.toList());
+	}
+
+	private ClienteListResponse(Cliente cliente) {
+		super();
+		this.idCliente = cliente.getIdCliente();
+		this.nomeCompleto = cliente.getNomeCompleto();
+		this.email = cliente.getEmail();
+		this.celular = cliente.getCelular();
+		this.cpf = cliente.getCpf();
 	}
 }

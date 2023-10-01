bf9a735a19faae7fd978bbab1ffa28193a6d36e6
Matheus Fagundes <matewsfagundes@gmail.com>
2023-10-01 15:09:53 -0300

    finaliza metodo cria servico

--- src/main/java/br/com/fagundespinturas/fagundespinturas/Servicos/infra/ServicoPinturaInfraRepository.java
+++ src/main/java/br/com/fagundespinturas/fagundespinturas/Servicos/infra/ServicoPinturaInfraRepository.java
@@ -4,15 +4,20 @@ import org.springframework.stereotype.Repository;
 
 import br.com.fagundespinturas.fagundespinturas.Servicos.api.repository.ServicoPinturaRepository;
 import br.com.fagundespinturas.fagundespinturas.Servicos.domain.ServicoPintura;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 @Repository
 @Log4j2
+@RequiredArgsConstructor
 public class ServicoPinturaInfraRepository implements ServicoPinturaRepository {
+
+	private final ServicoPinturaSpringDataJPARepository servicoPinturaSpringDataJPARepository;
 
 	@Override
 	public ServicoPintura salvaServico(ServicoPintura servicoPintura) {
 		log.info("[inicia] ServicoPinturaInfraRepository - salvaServico");
+		servicoPinturaSpringDataJPARepository.save(servicoPintura);
 		log.info("[finaliza] ServicoPinturaInfraRepository - salvaServico");
-		return null;
+		return servicoPintura;
 	}
 }

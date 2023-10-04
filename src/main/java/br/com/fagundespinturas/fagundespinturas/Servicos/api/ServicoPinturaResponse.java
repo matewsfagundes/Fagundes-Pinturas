d8fadd57bd695d18d4cf8c4cc97e15a4f3d9cb1a
matewsfagundes <matewsfagundes@gmail.com>
2023-10-03 21:34:42 -0300

    Revert "Feature/api servico"

--- src/main/java/br/com/fagundespinturas/fagundespinturas/Servicos/api/ServicoPinturaResponse.java
+++
@@ -1,9 +0,0 @@
-package br.com.fagundespinturas.fagundespinturas.Servicos.api;
-
-import java.util.UUID;
-
-import lombok.Value;
-@Value
-public class ServicoPinturaResponse {
-	private UUID idServicoPintura;
-}
--- src/main/java/br/com/fagundespinturas/fagundespinturas/cliente/domain/Sexo.java
+++
@@ -1,5 +0,0 @@
-package br.com.fagundespinturas.fagundespinturas.cliente.domain;
-
-public enum Sexo {
-	FEMININO, MASCULINO
-}
